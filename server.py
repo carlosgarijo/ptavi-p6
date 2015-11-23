caa3e73b0c1e598c9cbea44e89e6125675729067
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-11-23 10:30:17 +0100

    Con respuestas al INVITE

--- server.py
+++ server.py
@@ -23,13 +23,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             line_decode = line.decode('utf-8')
-            print("El cliente nos manda " + line_decode)
-            Metodo_rcv = line_decode.split(" ")[0]
-            if Metodo_rcv == "INVITE":
-                Answer_inv = "SIP/2.0 100 Trying\r\n\r\n"
-                Answer_inv += "SIP/2.0 180 Ring\r\n\r\n"
-                Answer_inv += "SIP/2.0 200 OK\r\n\r\n"
-                self.wfile.write(bytes(Answer_inv, 'utf-8'))
+            if line_decode:
+                print("El cliente nos manda " + line_decode)
+                Metodo_rcv = line_decode.split(" ")[0]
+                if Metodo_rcv == "INVITE":
+                    Answer_inv = "SIP/2.0 100 Trying\r\n\r\n"
+                    Answer_inv += "SIP/2.0 180 Ring\r\n\r\n"
+                    Answer_inv += "SIP/2.0 200 OK\r\n\r\n"
+                    self.wfile.write(bytes(Answer_inv, 'utf-8'))
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
