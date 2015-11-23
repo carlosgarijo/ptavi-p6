1cb0f296fe084acc5c74e63662c72423b5896bd4
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-11-23 10:10:34 +0100

    Iniciando server.py

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -15,17 +17,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
+        IP = self.client_address[0]
+        Metodos = ['INVITE', 'ACK', 'BYE']
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            line_decode = line.decode('utf-8')
+            print("El cliente nos manda " + line_decode)
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python server.py IP port audio_file")
+    if not os.path.exists(sys.argv[3]):
+        sys.exit("El archivo " + sys.argv[3] + " no existe")
+    PORT = int(sys.argv[2])
+    serv = socketserver.UDPServer(("", PORT), EchoHandler)
+    print("Listening...")
     serv.serve_forever()
