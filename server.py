3453e3141ec15d61304e271303214d74ed9fecd5
carlosgarijo <carlos.garijo.gyg@gmail.com>
2015-11-23 11:38:29 +0100

    Con todas las respuestas

--- server.py
+++ server.py
@@ -17,23 +17,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         #self.wfile.write(b"Hemos recibido tu peticion")
-        IP = self.client_address[0]
+        IP = str(self.client_address[0])
+        fichero_audio = sys.argv[3]
         #Metodos = ['INVITE', 'ACK', 'BYE']
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             line_decode = line.decode('utf-8')
             if line_decode:
-                print("El cliente nos manda " + line_decode)
-                Metodo_rcv = line_decode.split(" ")[0]
-                if Metodo_rcv == "INVITE":
-                    Answer = "SIP/2.0 100 Trying\r\n\r\n"
-                    Answer += "SIP/2.0 180 Ring\r\n\r\n"
-                    Answer += "SIP/2.0 200 OK\r\n\r\n"
-                elif Metodo_rcv == "ACK" or Metodo_rcv == "BYE":
-                    Answer = "SIP/2.0 200 OK\r\n\r\n"
+                request = line.split(" ")
+                if len(request) == 3:
+                    print("El cliente nos manda " + line_decode)
+                    Metodo_rcv = line_decode.split(" ")[0]
+                    if Metodo_rcv == "INVITE":
+                        Answer = "SIP/2.0 100 Trying\r\n\r\n"
+                        Answer += "SIP/2.0 180 Ring\r\n\r\n"
+                        Answer += "SIP/2.0 200 OK\r\n\r\n"
+                    elif Metodo_rcv == "ACK":
+                        
+                    elif Metodo_rcv == "BYE":
+                        Answer = "SIP/2.0 200 OK\r\n\r\n"
+                    else:
+                        Answer = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
                 else:
-                    Answer = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
+                    Answer = "SIP/2.0 400 Bad Request\r\n\r\n"
                 self.wfile.write(bytes(Answer, 'utf-8'))
             # Si no hay más líneas salimos del bucle infinito
             if not line:
